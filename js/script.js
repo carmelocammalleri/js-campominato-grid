2a247d9d0081077421efa9bbb013ee7ce1dcbaf0
Melozzo <carmecamma1995@gmail.com>
2023-09-18 15:55:34 +0200

    [edit]: cicle for and let containerGame

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//1.
+let containerGame = document.querySelector('.container');
+
+
+//2.
+  for(let i = 1; i <= 100; i++){
+    containerGame = document.createElement('div')
+    console.log(containerGame);
+  }
+   
