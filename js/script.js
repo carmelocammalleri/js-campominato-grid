b36ec8db9ff1b90aee4ecfa6fbaa3ea18ef327f6
Melozzo <carmecamma1995@gmail.com>
2023-09-18 16:48:21 +0200

    [add]: number in div box

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 //1.
 const containerGame = document.querySelector('.container');
+
 addBoxes();
 
 //2.
 function addBoxes (){
   for(let i = 1; i <= 100; i++){
-    let boxes = document.createElement('div');
-    boxes.classList.add('square');
-    containerGame.append(boxes);
-
+    let box = document.createElement('div');
+    box.classList.add('square');
+    containerGame.append(box);
     //4.
-    boxes.addEventListener('click', function() {
+    box.addEventListener('click', function() {
       this.classList.toggle('clicked');
-    })
+
+    //3.
+      box.innerHTML = [i];    
+  })
   }
-  
 }
 console.log(containerGame);
+//3.
+
+
 
