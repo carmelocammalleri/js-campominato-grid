1ba1ac6df8b1c1b9bf0d0fdc70606ca51b8b51fa
Melozzo <carmecamma1995@gmail.com>
2023-09-18 16:03:56 +0200

    [add]:  function container

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 //1.
 const containerGame = document.querySelector('.container');
-
+addBoxes();
 
 //2.
+function addBoxes (){
   for(let i = 1; i <= 100; i++){
     let boxes = document.createElement('div');
     boxes.classList.add('square');
     console.log(boxes);
     containerGame.append(boxes);
   }
+}
    
